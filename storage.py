ae9e3f6bdcd3e8877b77045887fac61a0196f5fa
stash93 <glebik.stanislav@gmail.com>
2014-10-14 00:07:03 -0700

    Better getTable method

--- storage.py
+++ storage.py
@@ -35,7 +35,33 @@ class Storage:
   def getId(self):
     return self.id
 
+  def getNamesForOneDimension(self, dimension, filters):
+    query = "select "
+    query += dimension.table + "." + dimension.field + " "
+    query += "from " + dimension.table
+    query += " where 1=1 "
+    for f in filters:
+      query += f.generateQueryString()
+
+    connection = sqlite3.connect(self.path)
+    res = connection.execute(query)
+    return map(lambda x: x[0], res)
+
+
   def getTable(self, dimensions, filters = []):
+    if len(dimensions) != 2:
+      fail("Bad dimensions size")
+
+    horizontalFilters = []
+    verticalFilters = []
+    for f in filters:
+      if f.dimension.name == self.dimensions[dimensions[0]].name:
+        horizontalFilters.append(f)
+      if f.dimension.name == self.dimensions[dimensions[1]].name:
+        verticalFilters.append(f)
+    horizontalNames = self.getNamesForOneDimension(self.dimensions[dimensions[0]], horizontalFilters)
+    verticalNames = self.getNamesForOneDimension(self.dimensions[dimensions[1]], verticalFilters)
+
     connection = sqlite3.connect(self.path)
     query = "select "
     for dimensionName in dimensions:
@@ -61,7 +87,18 @@ class Storage:
       d = resultDict.get(row[0], {})
       d[row[1]] = row[2]
       resultDict[row[0]] = d
-    return resultDict
+
+    ans = []
+    for h in horizontalNames:
+      last = []
+      for v in verticalNames:
+        if resultDict.has_key(h) and resultDict[h].has_key(v):
+          last.append(resultDict[h][v])
+        else:
+          last.append(0)
+      ans.append(last)
+
+    return horizontalNames, verticalNames, ans
 
 def parseConfigFile(filename="./config.xml"):
   dom = parse(filename)
